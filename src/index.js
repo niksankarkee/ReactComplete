01411e8e908abcd9776e98dafbe78b23b63e0d10
niksankarkee <karkeeniksan@gmail.com>
2020-09-28 20:22:23 +0900

    search component complited

--- src/index.js
+++ src/index.js
@@ -1,35 +1,5 @@
-// Import the React and ReactDOM libraries
-import React from 'react';
-import { render } from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
-import ApprovalCard from './ApprovalCard';
+import React from "react";
+import { render } from "react-dom";
+import App from "./App";
 
-
-// Create a react component
-const App = () => {
-    return (
-        <>
-            <div className="ui container comments">
-                <ApprovalCard >
-                    Are you sure you want to do this???
-                </ApprovalCard>
-
-                <ApprovalCard >
-                    <CommentDetail author='Sam' timeAgo="Today at 4:45PM" imageUrl={faker.image.avatar()} > This is awesome </CommentDetail>
-                </ApprovalCard>
-                <ApprovalCard >
-                    <CommentDetail author='Rijan' timeAgo="Today at 2:00AM" imageUrl={faker.image.avatar()} > This is Fantasitc </CommentDetail>
-                </ApprovalCard>
-                <ApprovalCard>
-                    <CommentDetail author='Roshan' timeAgo="Yesterday at 5:00PM" imageUrl={faker.image.avatar()} > I really enjoyed </CommentDetail>
-                </ApprovalCard>
-            </div>
-        </>
-
-    );
-};
-
-
-// Take the react component and show it on the screen
-render(<App />, document.querySelector('#root'));
+render(<App />, document.querySelector("#root"));
