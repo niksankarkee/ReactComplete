8ca6a7717786020fffddbc524fbe3ebed5a1fe3b
niksankarkee <karkeeniksan@gmail.com>
2020-10-04 17:50:08 +0900

    Simple redux concept

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,20 @@
-import React, { useState, useEffect } from "react";
-import SearchBar from "./SearchBar";
-import VideoList from "./VideoList";
-import VideoDetail from "./VideoDetail";
-import useVideos from "../hooks/useVideos";
+import React from "react";
+import SongList from "./SongList";
+import SongDetail from "./SongDetail";
 
 const App = () => {
-  const [selectedVideo, setSelectedVideo] = useState(null);
-  const [videos, search] = useVideos("nepali");
-
-  useEffect(() => {
-    setSelectedVideo(videos[0]);
-  }, [videos]);
-
   return (
-    <div className="ui container">
-      <SearchBar onFormSubmit={search} />
-      <div className="ui grid">
-        <div className="ui row">
-          <div className="eleven wide column">
-            <VideoDetail video={selectedVideo} />
-          </div>
-          <div className="five wide column">
-            <VideoList videos={videos} onVideoSelect={setSelectedVideo} />
-          </div>
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+        <div className="column eight wide">
+          <SongDetail />
         </div>
       </div>
     </div>
   );
 };
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 // Import the React and ReactDOM libraries
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
 import App from "./components/App";
+import reducers from "./reducers";
 
 // Take the react component and show it on the screen
-render(<App />, document.querySelector("#root"));
+render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
