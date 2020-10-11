6637db28873efee922665bba8b032f55bb3f2c30
niksankarkee <karkeeniksan@gmail.com>
2020-10-11 16:01:59 +0900

    Blog app completed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-// Import the React and ReactDOM libraries
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
-// Take the react component and show it on the screen
+const store = createStore(reducers, applyMiddleware(thunk));
 render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.querySelector("#root")
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,22 +1,8 @@
-import { combineReducers } from "redux";
-
-const songsReducer = () => {
-  return [
-    { title: "No Scrubs", duration: "4:05" },
-    { title: "Macarena", duration: "2:30" },
-    { title: "All Star", duration: "3:15" },
-    { title: "I Want it That Way", duration: "1:45" },
-  ];
-};
-
-const selectedSongReducer = (selectedSong = null, action) => {
-  if (action.type === "SONG_SELECTED") {
-    return action.payload;
-  }
-  return selectedSong;
-};
+import { combineReducers } from 'redux';
+import postsReducer from './postsReducer';
+import userReducer from './usersReducer';
 
 export default combineReducers({
-  songs: songsReducer,
-  selectedSong: selectedSongReducer,
+  posts: postsReducer,
+  users: userReducer,
 });
