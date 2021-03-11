57adcfe4f8197564ea181bd138f3b60843184894
niksankarkee <karkeeniksan@gmail.com>
2021-03-11 21:14:30 +0900

    video streaming incomplete

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-import React from "react";
-import { render } from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+import React from 'react';
+import { render } from 'react-dom';
 
-import App from "./components/App";
-import reducers from "./reducers";
+import App from './components/App';
 
-const store = createStore(reducers, applyMiddleware(thunk));
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.querySelector("#root")
-);
+render(<App />, document.querySelector('#root'));
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import postsReducer from './postsReducer';
-import userReducer from './usersReducer';
-
-export default combineReducers({
-  posts: postsReducer,
-  users: userReducer,
-});
