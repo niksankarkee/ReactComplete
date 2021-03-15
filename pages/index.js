4df06dba308ab9e7c09bc8b961a7a55c36cd429d
Niksan <k-niksan@net-e.co.jp>
2021-03-15 14:27:23 +0900

    initial setup

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,28 @@
-import EventList from '../components/events/event-list';
-import { getFeaturedEvents } from '../dummy-data';
+import path from 'path';
+import fs from 'fs/promises';
 
-const HomePage = () => {
-  const featuredEvents = getFeaturedEvents();
+function HomePage(props) {
+  const { products } = props;
+
   return (
-    <div>
-      <EventList items={featuredEvents} />
-    </div>
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
   );
-};
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return {
+    props: {
+      products: data.products,
+    },
+  };
+}
 
 export default HomePage;
